fb9ab740dcdfd89023821ced6e311fc6b56a269c
Emily Kiehl <emilydkiehl@gmail.com>
2018-07-22 04:04:16 -0400

    Update action.js

--- action.js
+++ action.js
@@ -2,5 +2,5 @@ $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 function logon() {
-    window.location = "https://emilydkiehl.github.io/Occurren/profilehome.html";
+    window.location = "https://emilydkiehl.github.io/Occurren/matches.html";
 }
