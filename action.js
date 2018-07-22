ea03b913295bf58f271e620ab1f405073920902e
Emily Kiehl <emilydkiehl@gmail.com>
2018-07-22 01:44:14 -0400

    Update action.js

--- action.js
+++ action.js
@@ -1,3 +1,6 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+function logon() {
+    document.getElementById("demo").style.color = "red";
+}
