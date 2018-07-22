cba61f5e2fe1bee4e31b4dbacef7222f63b0e597
Emily Kiehl <emilydkiehl@gmail.com>
2018-07-22 09:06:17 -0400

    Update action.js

--- action.js
+++ action.js
@@ -4,3 +4,35 @@ $('.message a').click(function(){
 function logon() {
     window.location = "https://emilydkiehl.github.io/Occurren/matches.html";
 }
+function addUsers(){
+   var users = [];
+   var passes = [];
+   
+   users.push(document.getElementById('user').value);
+   passes.push(document.getElementById('pass').value);
+   
+   window.location.href = 'https://emilydkiehl.github.io/Occurren/home.html';
+}
+function login(){
+   var i;
+   var userExists = new Boolean(false);
+   for (i = 0; i < users.length; i++) { 
+      if (users[i] = document.getElementById(username)){
+         userExists = true;
+      }
+      else{
+         userExists = false;
+         alert("Check to make sure your username is correct");
+      }
+   for (i = 0; i < users.length; i++) { 
+      if (passes[i] = document.getElementById(password)){
+         userExists = true;
+      }
+      else{
+         userExists = false;
+         alert("Check to make sure your password is correct");
+      }
+      if (userExists = true){
+         window.location.href = 'https://emilydkiehl.github.io/Occurren/home.html';
+      }
+}
